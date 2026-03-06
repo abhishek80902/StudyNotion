5fbf1cb3a5a686291caaf551472da37c11752437
Abhishek Kumar <kumarabhishek80902@gmail.com>
2026-03-06 13:32:01 +0530

    Refactor Navbar component

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -12,29 +12,25 @@ import { ACCOUNT_TYPE } from "../../utils/constants"
 import ProfileDropdown from "../core/Auth/ProfileDropDown"
 
 function Navbar() {
-
   const { token } = useSelector((state) => state.auth)
   const { user } = useSelector((state) => state.profile)
   const { totalItems } = useSelector((state) => state.cart)
-
   const location = useLocation()
 
   const [subLinks, setSubLinks] = useState([])
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    const fetchCategories = async () => {
+    ;(async () => {
       setLoading(true)
       try {
         const res = await apiConnector("GET", categories.CATEGORIES_API)
-        setSubLinks(res?.data?.data || [])
+        setSubLinks(res.data.data)
       } catch (error) {
         console.log("Could not fetch Categories.", error)
       }
       setLoading(false)
-    }
-
-    fetchCategories()
+    })()
   }, [])
 
   const matchRoute = (route) => {
@@ -43,26 +39,24 @@ function Navbar() {
 
   return (
     <div
-      className={`flex h-14 items-center justify-center border-b border-richblack-700 ${
+      className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
         location.pathname !== "/" ? "bg-richblack-800" : ""
-      }`}
+      } transition-all duration-200`}
     >
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
 
         {/* Logo */}
         <Link to="/">
-          <img src={logo} alt="Logo" width={160} height={32} loading="lazy"/>
+          <img src={logo} alt="Logo" width={160} height={32} loading="lazy" />
         </Link>
 
-        {/* Navigation Links */}
+        {/* Navigation links */}
         <nav className="hidden md:block">
           <ul className="flex gap-x-6 text-richblack-25">
-
-            {NavbarLinks.map((link, index) => {
-
-              if (link.title === "Catalog") {
-                return (
-                  <li key={index}>
+            {NavbarLinks.map((link, index) => (
+              <li key={index}>
+                {link.title === "Catalog" ? (
+                  <>
                     <div
                       className={`group relative flex cursor-pointer items-center gap-1 ${
                         matchRoute("/catalog/:catalogName")
@@ -72,117 +66,84 @@ function Navbar() {
                     >
                       <p>{link.title}</p>
                       <BsChevronDown />
-
-                      {/* Dropdown */}
-                      <div className="invisible absolute left-[50%] top-[50%] z-50 flex w-[250px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-200 group-hover:visible group-hover:translate-y-[1.5em] group-hover:opacity-100">
-
+                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
                         {loading ? (
                           <p className="text-center">Loading...</p>
-                        ) : subLinks?.length ? (
-                          subLinks
-                            .filter((subLink) => subLink?.courses?.length)
-                            .map((subLink, i) => (
-                              <Link
-                                key={i}
-                                to={`/catalog/${subLink.name
-                                  .split(" ")
-                                  .join("-")
-                                  .toLowerCase()}`}
-                                className="rounded-lg py-3 pl-4 hover:bg-richblack-50"
-                              >
-                                {subLink.name}
-                              </Link>
-                            ))
+                        ) : (subLinks && subLinks.length) ? (
+                          <>
+                            {subLinks
+                              ?.filter(
+                                (subLink) => subLink?.courses
+                              )
+                              ?.map((subLink, i) => (
+                                <Link
+                                  to={`/catalog/${subLink.name
+                                    .split(" ")
+                                    .join("-")
+                                    .toLowerCase()}`}
+                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                  key={i}
+                                >
+                                  <p>{subLink.name}</p>
+                                </Link>
+                              ))}
+                          </>
                         ) : (
                           <p className="text-center">No Courses Found</p>
                         )}
-
                       </div>
                     </div>
-                  </li>
-                )
-              }
-
-              // External Link (Generate Quiz)
-              if (link.external) {
-                return (
-                  <li key={index}>
-                    <a
-                      href={link.path}
-                      target="_self"
-                      rel="noopener noreferrer"
-                      className="font-semibold text-yellow-50 hover:text-yellow-25"
-                    >
-                      {link.title}
-                    </a>
-                  </li>
-                )
-              }
-
-              // Normal Link
-              return (
-                <li key={index}>
-                  <Link to={link.path}>
+                  </>
+                ) : (
+                  <Link to={link?.path}>
                     <p
-                      className={
-                        matchRoute(link.path)
+                      className={`${
+                        matchRoute(link?.path)
                           ? "text-yellow-25"
                           : "text-richblack-25"
-                      }
+                      }`}
                     >
                       {link.title}
                     </p>
                   </Link>
-                </li>
-              )
-
-            })}
-
+                )}
+              </li>
+            ))}
           </ul>
         </nav>
 
-        {/* Right Section */}
+        {/* Login / Signup / Dashboard */}
         <div className="hidden items-center gap-x-4 md:flex">
-
-          {/* Cart */}
-          {user && user.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
+          {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
             <Link to="/dashboard/cart" className="relative">
               <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
-
               {totalItems > 0 && (
-                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center rounded-full bg-richblack-600 text-xs font-bold text-yellow-100">
+                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
                   {totalItems}
                 </span>
               )}
             </Link>
           )}
-
-          {/* Login */}
           {token === null && (
             <Link to="/login">
-              <button className="rounded border border-richblack-700 bg-richblack-800 px-3 py-2 text-richblack-100">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
                 Log in
               </button>
             </Link>
           )}
-
-          {/* Signup */}
           {token === null && (
             <Link to="/signup">
-              <button className="rounded border border-richblack-700 bg-richblack-800 px-3 py-2 text-richblack-100">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
                 Sign up
               </button>
             </Link>
           )}
-
-          {/* Profile */}
           {token !== null && <ProfileDropdown />}
-
         </div>
 
-        {/* Mobile Menu */}
         <button className="mr-4 md:hidden">
-          <AiOutlineMenu fontSize={24} fill="#AFB2BF"/>
+          <AiOutlineMenu fontSize={24} fill="#AFB2BF" />
         </button>
 
       </div>
